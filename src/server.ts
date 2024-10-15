d10c1c2e069a072378a2d25c21acf36bcf849e16
uvais498 <muhammed.uvais@outlook.com>
2024-10-15 13:06:32 +0300

    t

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import Logger from './core/Logger';
-import { port } from './config';
+import { port, environment } from './config';
 import app from './app';
 
 app
   .listen(port, () => {
-    console.log(`server running on port : ${port}`);
     Logger.info(`server running on port : ${port}`);
+    console.log(environment);
   })
   .on('error', (e) => Logger.error(e));
